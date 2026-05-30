d51576376db31eacbb5d5ccb8bb67b5ecff2da49
Farhan-Aftab <91056029+Farhan-Aftab@users.noreply.github.com>
2026-05-30 14:17:18 +0500

    Update pa_ahn_normalization.py

--- pa_ahn_normalization.py
+++ pa_ahn_normalization.py
@@ -43,7 +43,7 @@ from norms.prior_anchored_adaptive_hybrid_normalization import AdaptiveHybridNor
 # =============================
 @dataclass
 class Config:
-    base_dir: str = "./PA-AHN"
+    base_dir: str = "./runs"
 
     # dataset_name: str = "cifar10"       # options: cifar10
     # num_classes: int = 10               # options: cifar10
